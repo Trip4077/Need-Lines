60785fe9503e08f4d3d41e6d21a367c6e5361b1c
Trip1701 <bj2336@email.vccs.edu>
2018-11-27 01:01:24 -0500

    Form Test 3

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //contact
-
-$('#contact__form').submit((event) => {
-  let name = document.querySelector('#inputName'),
-      email = document.querySelector('#inputEmail'),
-      message = document.querySelector('#inputMessage');
-
-      if(!name.value || !email.value || !message.value) {
-        alert('All Fields Required');
-      } else {
-        $.ajax({
-          url: "https://formspree.io/letslearntechyt@gmail.com",
-          method: "POST",
-          data: $('#contact__form').serialize(),
-          dataType: "json"
-        });
-        event.preventDefault();
-        $('#contact__form').get(0).reset();
-        alert('Message Sent');
-      }
-});
+// 
+// $('#contact__form').submit(function(event) {
+//   let name = document.querySelector('#inputName'),
+//       email = document.querySelector('#inputEmail'),
+//       message = document.querySelector('#inputMessage');
+//
+//       if(!name.value || !email.value || !message.value) {
+//         alert('All Fields Required');
+//       } else {
+//         $.ajax({
+//           url: "https://formspree.io/letslearntechyt@gmail.com",
+//           method: "POST",
+//           data: $(this).serialize(),
+//           dataType: "json"
+//         });
+//         event.preventDefault();
+//         $(this).get(0).reset();
+//         alert('Message Sent');
+//       }
+// });
