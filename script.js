a57d0ea00b08f7c64b2b57c2ef6a9225d8cb2d5d
Trip1701 <bj2336@email.vccs.edu>
2018-11-29 21:26:18 -0500

    Footer Form Data Transfer Implemented

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-//contact
-//
-// $('#contact__form').submit(function(event) {
-//   let name = document.querySelector('#inputName'),
-//       email = document.querySelector('#inputEmail'),
-//       message = document.querySelector('#inputMessage');
-//
-//       if(!name.value || !email.value || !message.value) {
-//         alert('All Fields Required');
-//       } else {
-//         $.ajax({
-//           url: "https://formspree.io/letslearntechyt@gmail.com",
-//           method: "POST",
-//           data: $(this).serialize(),
-//           dataType: "json"
-//         });
-//         event.preventDefault();
-//         $(this).get(0).reset();
-//         alert('Message Sent');
-//       }
-// });
+//Footer form
+
+document.querySelector('.footer__form button').addEventListener('click', () => {
+  const email = document.querySelector('#footer__form--email').value;
+  const name = document.querySelector('#footer__form--name').value;
+
+  sessionStorage.name = name;
+  sessionStorage.email = email;
+
+  url = './contact.html';
+
+  document.location.href = url;
+})
+
+if(sessionStorage.name) {
+  document.querySelector('#inputName').value = sessionStorage.name;
+}
+
+if(sessionStorage.email) {
+  document.querySelector('#inputEmail').value = sessionStorage.email;
+}
